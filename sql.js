0893386fbb83e74bf6bdb0df1b5a16adad4c7d22
Fucci <benjaminakacha@gmail.com>
2025-01-27 11:45:31 -0800

    comments

--- sql.js
+++ sql.js
@@ -4,15 +4,15 @@ const sql = require('mssql'); // SQL Server library
 
 // SQL Server connection configuration
 const sqlConfig = {
-    user: process.env.DB_USER,          // Your SQL Server username
-    password: process.env.DB_PASSWORD,  // Your SQL Server password
-    database: process.env.DB_NAME,      // Your database name
-    server: process.env.DB_SERVER,      // Your server name or IP
-    port: 1433, // Explicitly set port if needed
+    user: process.env.DB_USER,          //  SQL Server username
+    password: process.env.DB_PASSWORD,  //  SQL Server password
+    database: process.env.DB_NAME,      //  database name
+    server: process.env.DB_SERVER,      //  server name or IP
+    port: 1433, // Port
 
     options: {
-        encrypt: false,  // Use encrypt for production
-        trustServerCertificate: true    // Set to false in production if using a valid SSL cert
+        encrypt: false,  // TRUE IN PRODUCTION
+        trustServerCertificate: true    // FALSE IN PRODUCTION
     }
 };
 
